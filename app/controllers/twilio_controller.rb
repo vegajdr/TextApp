1c2e47de9ed34a698308cf1e7b2876488e80f162
vegajdr <jorgevp5@gmail.com>
2016-07-02 16:17:27 -0400

    Live text response

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,12 +1,18 @@
 class TwilioController < ApplicationController
 
-
+  skip_before_filter :authenticate_user!, :only => "incoming"
   after_filter :set_header
 
   skip_before_action :verify_authenticity_token
 
   def incoming
     slack_post
+    boot_twilio
+    sms = @client.messages.create(
+      from: '+19197565397',
+      to: params[:From],
+      body: "Your text has been posted to TIYTextAPP Slack Team."
+    )
   end
 
   private
@@ -23,6 +29,10 @@ class TwilioController < ApplicationController
         text: "#{params[:Body]}",
         as_user: true
       }
+    end
+
+    def boot_twilio
+      @client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
     end
 
 
--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,3 +1,7 @@
 class PhoneNumber < ActiveRecord::Base
   belongs_to :user
+
+  validates :phone_number, length: { minimum: 9 }
+
+  validates_presence_of :phone_number
 end
